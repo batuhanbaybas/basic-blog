3e60dd2946356aa24dc81b5c360080dbe4974ff4
batuhanbaybas <b.baybas@gmail.com>
2022-05-14 20:19:38 +0300

    page routes

---
+++ routes/pageRoutes/pageRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { getHomePage, getAboutPage, getPostPage, getNewPostPage } = require('../../controllers/pageControllers/pageControllers');
+
+
+
+const router = express.Router();
+
+
+
+exports.indexRoute = router.get('/',getHomePage );
+exports.aboutRoute = router.get('/about',getAboutPage);
+exports.postRoute = router.get('/post', getPostPage);
+exports.newPostRoute = router.get('/new_post',getNewPostPage);
+
+
+module.exports = router;
