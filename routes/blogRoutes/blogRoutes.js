31c7738fc0d9d969a7898a6b6f9db5a0aa6dd6c2
batuhanbaybas <b.baybas@gmail.com>
2022-05-14 21:31:50 +0300

    connect blog routes with blog controller

--- routes/blogRoutes/blogRoutes.js
+++ routes/blogRoutes/blogRoutes.js
@@ -1,16 +1,16 @@
-const express = require("express")
+import {getAllBlogs} from "../../controllers/blogControllers/blogController";
 
+const express = require("express")
+const blogController = require("../../controllers/blogControllers/blogController")
 
 const router = express.Router()
 
 
-
-exports.getAllBlogs = router.get("/")
-exports.getSinglePost = router.get("/post/:id")
-exports.updatePost = router.put("/post/:id/edit")
-exports.deletePost = router.delete("/post/:id/delete")
-exports.createPost = router.post("/post/new")
-
+exports.getAllBlogs = router.get("/", blogController.getAllBlogs)
+exports.getSinglePost = router.get("/post/:id", blogController.getSingleBlog)
+exports.updatePost = router.put("/post/:id/edit", blogController.updateBlog)
+exports.deletePost = router.delete("/post/:id/delete", blogController.deleteBlog)
+exports.createPost = router.post("/post/new", blogController.createBlog)
 
 
 export default router
